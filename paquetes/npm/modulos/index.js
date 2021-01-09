1ffe18ce07360d3dd5d1e6911581f7c35523314f
Francisco Javier Maldonado Hernandez <hellopaco9@gmail.com>
2021-01-09 14:24:01 -0600

    Paquetes

---
+++ paquetes/npm/modulos/index.js
@@ -0,0 +1,6 @@
+// traer nuestro modulo
+// Ejecutar una funcion de nuestro modulos
+const modulo = require("./modulo");
+
+console.log(modulo.saludar());
+// modulo.saludar;
---
+++ paquetes/npm/modulos/modulo.js
@@ -0,0 +1,8 @@
+function saludar() {
+  console.log("Hola mundo perros");
+}
+
+module.exports = {
+  saludar,
+  prop1: "Hola",
+};
